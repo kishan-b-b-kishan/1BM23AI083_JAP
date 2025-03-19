54c9df3aedd5b3dbc4c0c4c957cd4ea4c221ea7c
kishan-b-b-kishan <kishanb.ai23@bmsce.ac.in>
2025-03-19 15:21:31 +0530

    Add files via upload

---
+++ _2a.java
@@ -0,0 +1,33 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class _2a {
+    public static void main(String[] args) {
+        int ia = 9000;
+        Integer ib = ia;
+        int ic = ib;
+        System.out.println("Value of a int  = "+ia);
+        System.out.println("Value of b int= "+ib);
+        System.out.println("Value of c int= "+ic);
+
+        char ca = 'a';
+        Character cb = ca;
+        char cc = cb;
+        System.out.println("Value of a char = "+ca);
+        System.out.println("Value of b char = "+cb);
+        System.out.println("Value of c char = "+cc);
+
+        double da = 10.5;
+        Double db = da;
+        double dc = db;
+        System.out.println("Value of a double = "+da);
+        System.out.println("Value of b  double= "+db);
+        System.out.println("Value of c double= "+dc);
+
+        float fa =2.05;
+        Float fb = fa;
+        float fc = fb;
+        System.out.println("Value of a float = "+fa);
+        System.out.println("Value of b float  = "+fb);
+        System.out.println("Value of c float = "+fc);
+    }
+}
